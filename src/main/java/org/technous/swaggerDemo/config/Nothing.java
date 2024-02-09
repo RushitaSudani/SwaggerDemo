19f845f8471308bbacdf42d041fd8251e383f3ac
Tisha Sudani <ashok.svit@gmail.com>
2024-02-09 12:38:14 +0530

    nothing addedd

---
+++ src/main/java/org/technous/swaggerDemo/config/Nothing.java
@@ -0,0 +1,7 @@
+package org.technous.swaggerDemo.config;
+
+public class Nothing {
+    void test(){
+        System.out.println("here is nothing....");
+    }
+}
--- src/main/java/org/technous/swaggerDemo/config/Test.java
+++ src/main/java/org/technous/swaggerDemo/config/Test.java
@@ -1,4 +1,7 @@
 package org.technous.swaggerDemo.config;
 
 public class Test {
+    public void test(){
+        System.out.println("test...");
+    }
 }
